7fc54c5030fa295478aa18da9e750c4a40e32ff1
poolan <raulpoolan@gmail.com>
2021-10-04 15:11:21 +0300

    admin

--- src/main/java/com/example/bookstorefall2021/Bookstorefall2021Application.java
+++ src/main/java/com/example/bookstorefall2021/Bookstorefall2021Application.java
@@ -11,18 +11,18 @@ import com.example.bookstorefall2021.model.Book;
 import com.example.bookstorefall2021.model.Bookrepository;
 import com.example.bookstorefall2021.model.Category;
 import com.example.bookstorefall2021.model.CategoryRepository;
-
-
+import com.example.bookstorefall2021.model.User;
+import com.example.bookstorefall2021.model.UserRepository;
 
 @SpringBootApplication
 public class Bookstorefall2021Application {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(Bookstorefall2021Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Bookstorefall2021Application.class, args);
 	}
-	
+
 	@Bean
 	public CommandLineRunner bookDemo(Bookrepository repository, CategoryRepository crepository) {
 		return (args) -> {
@@ -30,10 +30,12 @@ public class Bookstorefall2021Application {
 			crepository.save(new Category("Horror"));
 			crepository.save(new Category("Drama"));
 			crepository.save(new Category("Romance"));
-			
-			repository.save(new Book("Madman", "John", 1957, "0-4944-4261-1", 14.99, crepository.findByName("Romance").get(0)));
-			repository.save(new Book("The Moon", "Peter", 2015, "0-6270-4420-4", 49.99, crepository.findByName("Horror").get(0)));	
-			
+
+			repository.save(
+					new Book("Madman", "John", 1957, "0-4944-4261-1", 14.99, crepository.findByName("Romance").get(0)));
+			repository.save(new Book("The Moon", "Peter", 2015, "0-6270-4420-4", 49.99,
+					crepository.findByName("Horror").get(0)));
+
 			log.info("fetch all books");
 			for (Book book : repository.findAll()) {
 				log.info(book.toString());
--- src/main/java/com/example/bookstorefall2021/web/BookController.java
+++ src/main/java/com/example/bookstorefall2021/web/BookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,64 +21,69 @@ import com.example.bookstorefall2021.model.Book;
 import com.example.bookstorefall2021.model.Bookrepository;
 import com.example.bookstorefall2021.model.CategoryRepository;
 
-
 @Controller
 public class BookController {
 	@Autowired
 	private Bookrepository repository;
-	
+
 	@Autowired
-	private CategoryRepository crepository; 
+	private CategoryRepository crepository;
 
 	@RequestMapping(value = "/index", method = RequestMethod.POST)
 	public String showIndex() {
 		return "index";
 	}
-	
-	 @RequestMapping(value={ "/", "/booklist"})
-	    public String bookList(Model model) {	
-	        model.addAttribute("books", repository.findAll());
-	        return "booklist";
-	    }
-	 
+
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+
+	@RequestMapping(value = { "/", "/booklist" })
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
+
 	// RESTful service to get all books
-	    @RequestMapping(value="/books", method = RequestMethod.GET)
-	    public @ResponseBody List<Book> studentListRest() {	
-	        return (List<Book>) repository.findAll();
-	    }    
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> studentListRest() {
+		return (List<Book>) repository.findAll();
+	}
 
-		// RESTful service to get book by id
-	    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
-	    public @ResponseBody Optional<Book> findStudentRest(@PathVariable("id") Long bookId) {	
-	    	return repository.findById(bookId);
-	    }  
-	 
-	 @RequestMapping(value = "/add")
-	    public String addBook(Model model){
-	    	model.addAttribute("books", new Book());
-	    	model.addAttribute("categories", crepository.findAll());
-	        return "addbook";
-	    } 
-	 @RequestMapping(value = "/save", method = RequestMethod.POST)
-		public String save(Book book) {
-			repository.save(book);
-			return "redirect:booklist";
-		}
+	// RESTful service to get book by id
+	@RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findStudentRest(@PathVariable("id") Long bookId) {
+		return repository.findById(bookId);
+	}
 
-		@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-		public String deleteBook(@PathVariable("id") Long studentId, Model model) {
-			repository.deleteById(studentId);
-			return "redirect:../booklist";
-		}
-		
-		@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-		public String editBook(@PathVariable("id") Long bookId, Model model) {
-			
-			Optional<Book> book = repository.findById(bookId);
-			model.addAttribute("books", repository.findById(bookId));	
-			model.addAttribute("categories", crepository.findAll());
-			return "modifybook";
-		}
-	 
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("books", new Book());
+		model.addAttribute("categories", crepository.findAll());
+		return "addbook";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+
+	@PreAuthorize("hasRole('ADMIN')")
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+
+		Optional<Book> book = repository.findById(bookId);
+		model.addAttribute("books", repository.findById(bookId));
+		model.addAttribute("categories", crepository.findAll());
+		return "modifybook";
+	}
 
 }
